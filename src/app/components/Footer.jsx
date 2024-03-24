8b22f04ca3760ad1812d87bc66cba92075b41f40
Appleeyes <appleeyespackage@gmail.com>
2024-03-24 01:23:57 +0100

    new commit

--- src/app/components/Footer.jsx
+++ src/app/components/Footer.jsx
@@ -16,29 +16,29 @@ export const Footer = () => {
 
                     <div>
                         <ul className='no-head'>
-                            <li><a href="#/about">About</a></li>
-                            <li><a href="#/jobs">Jobs</a></li>
-                            <li><a href="#/record">For the Record</a></li>
+                            <li><a href="/about">About</a></li>
+                            <li><a href="/jobs">Jobs</a></li>
+                            <li><a href="/record">For the Record</a></li>
                         </ul>
                     </div>
 
                     <div> 
                         <ul>
                            <h5>COMMUNITIES</h5>
-                            <li><a href="#/artists">For Artists</a></li>
-                            <li><a href="#/dev">Developers</a></li>
-                            <li><a href="#/advert">Advertising</a></li>
-                            <li><a href="#/investors">Investors</a></li>
-                            <li><a href="#/vendors">Vendors</a></li>
+                            <li><a href="/artists">For Artists</a></li>
+                            <li><a href="/dev">Developers</a></li>
+                            <li><a href="/advert">Advertising</a></li>
+                            <li><a href="/investors">Investors</a></li>
+                            <li><a href="/vendors">Vendors</a></li>
                         </ul>
                     </div>
 
                     <div>
                         <ul>
                             <h5>USEFUL LINKS</h5>
-                            <li><a href="#/support">Support</a></li>
-                            <li><a href="#/web">Web Player</a></li>
-                            <li><a href="#/mobile">Free Mobile App</a></li>
+                            <li><a href="/support">Support</a></li>
+                            <li><a href="/web">Web Player</a></li>
+                            <li><a href="/mobile">Free Mobile App</a></li>
                         </ul>
                     </div>
                 </div>
@@ -51,10 +51,10 @@ export const Footer = () => {
             <div className='down'>
                 <div>
                     <ul>
-                        <li><a href="#/privacy">Privacy Center</a></li>
-                        <li><a href="#/policy">Privacy Policy</a></li>
-                        <li><a href="#/cookies">Cookies</a></li>
-                        <li><a href="#/about">About Ads</a></li>
+                        <li><a href="/privacy">Privacy Center</a></li>
+                        <li><a href="/policy">Privacy Policy</a></li>
+                        <li><a href="/cookies">Cookies</a></li>
+                        <li><a href="/about">About Ads</a></li>
                     </ul>
                 </div>
                 <div className='left'>
